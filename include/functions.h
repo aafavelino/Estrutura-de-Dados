bbdc545fb5da34b1fb4c7010fa0b7b26fddff573
Adelino Afonso <adelino-afonso@hotmail.com>
2016-03-09 20:14:45 -0300

    Update functions.h

--- include/functions.h
+++ include/functions.h
@@ -4,33 +4,30 @@
 using namespace std;
 
 template <typename Obj, typename Obj2>
-	int wrapper_bsearch(Obj vetor, Obj2 x, int e, int d);
+	int wrapper_bsearch(Obj vetor, Obj2 x, int e, int d, int k);
 
 template <typename Obj, typename Obj2>
-	long int busca_sequencial_iterativa(Obj &vetor, Obj2 x, int e, int d);
+	long int busca_sequencial_iterativa(Obj &vetor, Obj2 x, int e, int d, Obj2 k);
 
 template <typename Obj, typename Obj2>
-	long int busca_binaria_iterativa(vector <Obj> vetor, Obj2 x, int e, int d);
+	long int busca_binaria_iterativa(Obj &vetor, Obj2 x, int e, int d, int k);
 
 template <typename Obj, typename Obj2>
-	long int busca_ternaria_recursiva(Obj &vetor, Obj2 x, int e, int d);
+	long int busca_ternaria_recursiva(Obj &vetor, Obj2 x, int e, int d, int k);
 	
 	int compare(const void *x, const void *y);
 
 	//IRENE...
 template <typename Obj, typename Obj2>
-	int busca_binaria_recursiva( Obj &vetor, Obj2 x, int e, int d);
+	int busca_binaria_recursiva( Obj &vetor, Obj2 x, int e, int d, int k);
 
 	void randomFill( std::vector<int> &V, const int lower, const int upper, const unsigned int seed);
 
 template <typename Obj, typename Obj2>
-	long int busca_sequencial_recursiva(Obj &vetor, Obj2 x, int e, int d);
+	long int busca_sequencial_recursiva(Obj &vetor, Obj2 x, int e, int d, Obj2 k);
 
 template <typename Obj, typename Obj2>
-	int busca_ternaria_iterativa( Obj &vetor, Obj2 x, int e, int d);
-//Falta Implementar...
-template <typename Obj, typename Obj2>
-	int wrapper_search( Obj &vetor, Obj2 x, int e, int d);
+	int busca_ternaria_iterativa( Obj &V, Obj2 x, int e, int d, int k);
 
 template <typename C>
 	int compara_search(const C& V, const C& b);
